05724a699e92a2c695db77497c1fef24efb961c4
Uriel Aispuro <urielalexis64@gmail.com>
2021-12-26 21:39:17 -0700

    add Swipeable actions

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import {
+	LeadingActions,
+	SwipeableList,
+	SwipeableListItem,
+	SwipeAction,
+	TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
 import {formatDate} from "../helpers";
 
 import SalaryIcon from "../img/icono_ahorro.svg";
@@ -19,23 +26,43 @@ const iconsDictionary = {
 	subscriptions: SubscriptionsIcon,
 };
 
-const Expense = ({expense}) => {
-	const {category, name, amount, id, date} = expense;
+const Expense = ({expense, setEditExpense, handleRemoveExpense}) => {
+	const {category, name, amount, date} = expense;
+
+	const leadingActions = () => (
+		<LeadingActions>
+			<SwipeAction onClick={() => setEditExpense(expense)}>Edit</SwipeAction>
+		</LeadingActions>
+	);
+
+	const trailingActions = () => (
+		<TrailingActions>
+			<SwipeAction destructive onClick={() => handleRemoveExpense(expense.id)}>
+				Delete
+			</SwipeAction>
+		</TrailingActions>
+	);
 
 	return (
-		<div className='gasto sombra'>
-			<div className='contenido-gasto'>
-				<img src={iconsDictionary[category]} alt={category} />
-				<div className='descripcion-gasto'>
-					<p className='categoria'>{category}</p>
-					<p className='nombre-gasto'>{name}</p>
-					<p className='fecha-gasto'>
-						Agregado el <span>{formatDate(date)}</span>
-					</p>
+		<SwipeableList>
+			<SwipeableListItem
+				leadingActions={leadingActions()}
+				trailingActions={trailingActions()}>
+				<div className='gasto sombra'>
+					<div className='contenido-gasto'>
+						<img src={iconsDictionary[category]} alt={category} />
+						<div className='descripcion-gasto'>
+							<p className='categoria'>{category}</p>
+							<p className='nombre-gasto'>{name}</p>
+							<p className='fecha-gasto'>
+								Agregado el <span>{formatDate(date)}</span>
+							</p>
+						</div>
+					</div>
+					<p className='cantidad-gasto'>${amount}</p>
 				</div>
-			</div>
-			<p className='cantidad-gasto'>${amount}</p>
-		</div>
+			</SwipeableListItem>
+		</SwipeableList>
 	);
 };
 
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,11 +1,16 @@
 import Expense from "./Expense";
 
-const ExpenseList = ({expenses}) => {
+const ExpenseList = ({expenses, setEditExpense, handleRemoveExpense}) => {
 	return (
 		<div className='listado-gastos contenedor'>
 			<h2>{expenses.length ? "Expenses" : "There is no expenses yet"}</h2>
 			{expenses.map((expense) => (
-				<Expense key={expense.id} expense={expense} />
+				<Expense
+					key={expense.id}
+					expense={expense}
+					setEditExpense={setEditExpense}
+					handleRemoveExpense={handleRemoveExpense}
+				/>
 			))}
 		</div>
 	);
